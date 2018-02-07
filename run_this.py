f48a379d45767a4dbedf77d65a65a32f18eea5aa
mikelee1 <liyuanye123456@gmail.com>
2018-02-07 18:27:50 +0800

    rejust nn and like simplest maze

--- run_this.py
+++ run_this.py
@@ -16,13 +16,13 @@ import pygame
 
 GAME = 'bird'  # the name of the game being played for log files
 ACTIONS = 4  # number of valid actions
-GAMMA = 0.8  # decay rate of past observations
-OBSERVE = 500.  # timesteps to observe before training
-EXPLORE = 20000.  # frames over which to anneal epsilon
+GAMMA = 0.1  # decay rate of past observations
+OBSERVE = 5000.  # timesteps to observe before training
+EXPLORE = 200000.  # frames over which to anneal epsilon
 FINAL_EPSILON = 0.01  # final value of epsilon
-INITIAL_EPSILON = 1#0.01  # starting value of epsilon
+INITIAL_EPSILON = 0.01#0.01  # starting value of epsilon
 REPLAY_MEMORY = 50000  # number of previous transitions to remember
-BATCH = 10  # size of minibatch
+BATCH = 32  # size of minibatch
 FRAME_PER_ACTION = 1
 
 
@@ -46,36 +46,36 @@ def max_pool_2x2(x):
 
 def createNetwork():
     # network weights
-    W_conv1 = weight_variable([8, 8, 4, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([8, 8, 4, 64])
+    b_conv1 = bias_variable([64])
 
-    W_conv2 = weight_variable([4, 4, 32, 64])
+    W_conv2 = weight_variable([2, 2, 64, 64])
     b_conv2 = bias_variable([64])
 
     W_conv3 = weight_variable([3, 3, 64, 64])
     b_conv3 = bias_variable([64])
 
-    W_fc1 = weight_variable([1600, 512])
+    W_fc1 = weight_variable([256, 512])
     b_fc1 = bias_variable([512])
 
     W_fc2 = weight_variable([512, ACTIONS])
     b_fc2 = bias_variable([ACTIONS])
 
     # input layer
-    s = tf.placeholder("float", [None, 80, 80, 4])
+    s = tf.placeholder("float", [None, 12, 12, 4])
 
     # hidden layers
     h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 1) + b_conv2)
     # h_pool2 = max_pool_2x2(h_conv2)
 
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
     # h_pool3 = max_pool_2x2(h_conv3)
 
     # h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
-    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+    h_conv3_flat = tf.reshape(h_conv3, [-1, 256])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
@@ -86,19 +86,25 @@ def createNetwork():
 
 
 def generatepos():
-    pos = [20*random.randint(0,6),20*random.randint(0,5)]
+    pos = [20*random.randint(0,6),20*random.randint(0,6)]
     return pos
 
+def generatetreapos():
+    pos = [20*random.randint(0,6),20*random.randint(0,6)]
+    return pos
+import math
+def sigmoid(x):
+    return 2 / (1 + math.exp(-x)) - 1
 
-def trainNetwork(s, readout, h_fc1, sess):
+def trainNetwork(s, readout, h_fc1, sess):#readout [None,4]
     # define the cost function
     snackpos = generatepos()
-    treapos = generatepos()
+    treapos = generatetreapos()
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
     readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
     cost = tf.reduce_mean(tf.square(y - readout_action))
-    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
+    train_step = tf.train.GradientDescentOptimizer(1e-2).minimize(cost)
 
     # open up a game state to communicate with emulator
     game_state = game.envstate(treapos,snackpos)
@@ -106,16 +112,15 @@ def trainNetwork(s, readout, h_fc1, sess):
     # store the previous observations in replay memory
     D = deque()
 
-    # printing
-
 
     # get the first state by doing nothing and preprocess the image to 80x80x4
     do_nothing = np.zeros(ACTIONS)
     do_nothing = 'left'
     x_t, r_0, terminal,snackpos = game_state.step(do_nothing,snackpos)
-    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+    x_t = cv2.cvtColor(cv2.resize(x_t, (12, 12)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, 3)
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+
 
     # saving and loading networks
     saver = tf.train.Saver()
@@ -132,14 +137,16 @@ def trainNetwork(s, readout, h_fc1, sess):
     t = 0
     dic = {0:'up',1: 'down', 2: 'left',3:'right'}
     dic1 = {'up':[1,0,0,0], 'down':[0,1,0,0], 'left':[0,0,1,0],'right':[0,0,0,1]}
+    total =0
     while "flappy bird" != "angry bird":
         # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s: [s_t]})[0]
-        a_t = np.zeros([ACTIONS])
-        action_index = 0
+        aaa = readout.eval(feed_dict={s: [s_t]})
+        readout_t = aaa[0]
+
+
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
-                print("----------Random Action----------")
+                # print("----------Random Action----------")
                 action_index = random.randrange(ACTIONS)
                 a_t=dic[random.randrange(ACTIONS)]
             else:
@@ -151,22 +158,27 @@ def trainNetwork(s, readout, h_fc1, sess):
 
         # scale down epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
-            print('minus epsilon')
+
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         # run the selected action and observe next state and reward
         x_t1_colored, r_t, terminal,snackpos = game_state.step(a_t,snackpos)
-        x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
-        # cv2.imwrite('test.jpg',x_t1)
+        x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (12, 12)), cv2.COLOR_BGR2GRAY)
 
-        ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
-        x_t1 = np.reshape(x_t1, (80, 80, 1))
-        # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
-        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
+        ret, x_t1 = cv2.threshold(x_t1, 1, 255, 3)
+        # cv2.imwrite('test' + str(t) + '.jpg', x_t1_colored)
+        # cv2.imwrite('testwioutcolor'+str(t)+'.jpg',x_t1)
+        #x_t1 = np.reshape(x_t1, (80, 80, 1))
+
+        #s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+        s_t1 = np.stack((x_t1,x_t1,x_t1,x_t1),axis=2)
+
+        # if r_t >100 or r_t < -100:
+        #     r_t =max(-99.99999,min(99.99999,r_t))
         # store the transition in D
-        if r_t!=0:
-            D.append((s_t, a_t, r_t, s_t1, terminal))
+
+        D.append((s_t, a_t, r_t, s_t1, terminal,readout_t[action_index]))   #readout_t[action_index] is the max predict q value of nn at time t
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
@@ -181,29 +193,39 @@ def trainNetwork(s, readout, h_fc1, sess):
             a_batch = [dic1[d[1]] for d in minibatch]
             r_batch = [d[2] for d in minibatch]
             s_j1_batch = [d[3] for d in minibatch]
-
+            old = [d[5] for d in minibatch]
             y_batch = []
             readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
-                if terminal:
+                if terminal or terminal == 4:#todo
                     y_batch.append(r_batch[i])
                 else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                    procdata = sigmoid(np.max(readout_j1_batch[i]))
+                    tmpdata = old[i]+0.1*(r_batch[i] + GAMMA * procdata-old[i])
+                    tmpdata =sigmoid(tmpdata)
+                    y_batch.append(tmpdata)
             # perform gradient step
+
             train_step.run(feed_dict={
                 y: y_batch,
                 a: a_batch,
                 s: s_j_batch}
             )
+            total += sess.run(cost, feed_dict={y: y_batch, a: a_batch, s: s_j_batch})
+            if t%1000 == 0:
+                result = total/1000.0
+                print('loss at time %s:%s',(t,result,epsilon))
+                total =0
 
         # update the old values
         s_t = s_t1
         t += 1
 
         # save progress every 10000 iterations
-        if t % 500 == 0:
+        if t % 2000 == 0:
+
             saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
 
         # print info
@@ -214,8 +236,11 @@ def trainNetwork(s, readout, h_fc1, sess):
             state = "explore"
         else:
             state = "train"
-
-        print("TIMESTEP", t, "/ STATE", state,"/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,"/ Q_MAX %e" % np.max(readout_t))
+        if terminal:
+            x1 = random.randint(0,6)
+            y1 = random.randint(0,6)
+            snackpos = [x1*20, y1*20]
+        # print("TIMESTEP", t, "/ STATE", state,"/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,"/ Q_MAX %e" % np.max(readout_t))
         # write info to files
         '''
         if t % 10000 <= 100:
--- snack.py
+++ snack.py
@@ -21,7 +21,7 @@ class envstate:
         pygame.init()
         self.fpsClock = pygame.time.Clock()
         # 创建pygame显示层
-        self.playSurface = pygame.display.set_mode((120,100))
+        self.playSurface = pygame.display.set_mode((120,120))
         pygame.display.set_caption('Raspberry Snake')
 
         # 初始化变量
@@ -31,12 +31,13 @@ class envstate:
         self.raspberrySpawned = 1
         self.direction = 'right'
         self.changeDirection = self.direction
-        self.terminal = False
+
 
     def reset(self,trea,s):
         self.__init__(trea,s)
 
     def step(self,action,s=[20,20]):
+        self.terminal = False
         self.snakePosition=s[:]
         self.reward = 0
         # 检测例如按键等pygame事件
@@ -85,11 +86,11 @@ class envstate:
         self.snakeSegments.pop()
         # 如果吃掉树莓，则重新生成树莓
         if self.raspberrySpawned == 0:
-            new1 = self.snakePosition[0]/20
-            new2 = self.snakePosition[1]/20
-            self.reward = 100
-            self.x = random.randrange(0,6)
-            self.y = random.randrange(0,5)
+            self.terminal = 4
+            print('got one')
+            self.reward = 1
+            self.x = random.randint(0,6)
+            self.y = random.randint(0,6)
 
             self.raspberryPosition = [int(self.x*20),int(self.y*20)]
             self.raspberrySpawned = 1
@@ -97,25 +98,35 @@ class envstate:
         self.playSurface.fill(blackColour)
         for position in self.snakeSegments:
             pygame.draw.rect(self.playSurface,whiteColour,Rect(position[0],position[1],20,20))
-            pygame.draw.rect(self.playSurface,redColour,Rect(self.raspberryPosition[0], self.raspberryPosition[1],20,20))
+            pygame.draw.rect(self.playSurface,greyColour,Rect(self.raspberryPosition[0], self.raspberryPosition[1],20,20))
 
         # 刷新pygame显示层
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
 
         # 判断是否死亡
         if self.snakePosition[0] > 100 or self.snakePosition[0] < 0:
-            self.reward = -100
+            x1 = random.randint(0,6)
+            y1 = random.randint(0,6)
+            x2 = random.randint(0,6)
+            y2 = random.randint(0,6)
+            self.reset([x1*20,y1*20],[x2*20,y2*20])
+            self.reward = -1
             self.terminal = True
-            self.reset([60,60],[40,40])
-        if self.snakePosition[1] > 80 or self.snakePosition[1] < 0:
+
+        if self.snakePosition[1] > 100 or self.snakePosition[1] < 0:
             # for snakeBody in self.snakeSegments[1:]:
             #     if self.snakePosition[0] == snakeBody[0] and self.snakePosition[1] == snakeBody[1]:
+            x1 = random.randint(0,6)
+            y1 = random.randint(0,6)
+            x2 = random.randint(0,6)
+            y2 = random.randint(0,6)
+            self.reset([x1*20,y1*20],[x2*20,y2*20])
             self.terminal = True
-            self.reward=-100
-            self.reset([60,60],[40,40])
+            self.reward=-1
 
         # 控制游戏速度
-        self.fpsClock.tick(50)
-        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        self.fpsClock.tick(5)
+
         pygame.display.flip()
         return image_data,self.reward,self.terminal,self.snakePosition
 
@@ -130,7 +141,7 @@ class envstate:
         gameOverRect.midtop = (320, 10)
         playSurface.blit(gameOverSurf, gameOverRect)
         pygame.display.flip()
-        time.sleep(5)
+        time.sleep(50)
         pygame.quit()
         sys.exit()
 
